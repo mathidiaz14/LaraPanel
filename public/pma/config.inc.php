3e83f57d6caa7382c79678338fe7ee46332c9852
Mathias Díaz <diaz.mathias.14@gmail.com>
2026-07-03 12:18:40 -0300

    Modificaciones en phpmyadmin

--- public/pma/config.inc.php
+++ public/pma/config.inc.php
@@ -4,7 +4,7 @@ $i = 0;
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'signon';
 $cfg['Servers'][$i]['SignonSession'] = 'SignonSession';
-// $cfg['Servers'][$i]['SignonURL'] = '/login'; // Si falla, va al panel
+$cfg['Servers'][$i]['SignonURL'] = '/login';
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
